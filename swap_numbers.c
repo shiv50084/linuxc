9af6620fb233f2044ea52e9f58b5e69d1c337c9a
mygentoo <M_O_Bz@163.com>
2016-03-25 15:04:29 +0000

    traps in use ^ to swap two number

--- swap_numbers.c
+++ swap_numbers.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+void myswap(int *a, int *b)
+{
+	/*
+	 * 这里面却存在着一个非常隐蔽的陷阱
+	 * 对于arr[n]中如果arr[i] == arr[j]
+	 * 在交换arr[i]和arr[j]的时候需要确保这两个数不是同一个数
+	 * 否则下面的代码就变成了
+	 * a ^= b
+	 * a ^= b
+	 * a ^= b
+	 * 把同一个数异或了3次
+	 */
+	if (a == b)
+		return;
+
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
+}
+
 int main(int argc, char **argv)
 {
 	int a, b;
@@ -10,13 +30,8 @@ int main(int argc, char **argv)
 
 	printf("a = %d, b = %d\n", a, b);
 
-	printf("swap them:");
-	/*
-	 * this is how to swap two number without use tmp var
-	 */
-	a = a ^ b;
-	b = b ^ a;
-	a = a ^ b;
+	printf("swap them:\n");
+	myswap(&a, &b);
 
 	printf("a = %d, b = %d\n", a, b);
 
