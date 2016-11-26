dbcf8a736086bc716e903393de9c00b73587819b
zeroway <M_O_Bz@163.com>
2016-11-26 23:57:07 +0000

    kernel list: add list_del

--- kernel_list/main.c
+++ kernel_list/main.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+# define POISON_POINTER_DELTA 0
+#define LIST_POISON1  ((void *) 0x100 + POISON_POINTER_DELTA)
+#define LIST_POISON2  ((void *) 0x200 + POISON_POINTER_DELTA)
+
 struct list_head {
 	struct list_head *next, *prev;
 };
 
-static void __list_add(struct list_head *new,
+static inline void __list_add(struct list_head *new,
 		struct list_head *prev,
 		struct list_head *next)
 {
@@ -15,6 +19,57 @@ static void __list_add(struct list_head *new,
 	new->next = next;
 	new->prev = prev;
 	prev->next = new;
+}
+
+typedef __signed__ char __s8;
+typedef unsigned char __u8;
+
+typedef __signed__ short __s16;
+typedef unsigned short __u16;
+
+typedef __signed__ int __s32;
+typedef unsigned int __u32;
+
+typedef __signed__ long __s64;
+typedef unsigned long __u64;
+
+/* The "volatile" is due to gcc bugs */
+#define barrier() __asm__ __volatile__("": : :"memory")
+
+static __always_inline void __write_once_size(volatile void *p, void *res, int size)
+{
+	switch (size) {
+	case 1: *(volatile __u8 *)p = *(__u8 *)res; break;
+	case 2: *(volatile __u16 *)p = *(__u16 *)res; break;
+	case 4: *(volatile __u32 *)p = *(__u32 *)res; break;
+	case 8: *(volatile __u64 *)p = *(__u64 *)res; break;
+	default:
+		barrier();
+		__builtin_memcpy((void *)p, (const void *)res, size);
+		barrier();
+	}
+}
+
+#define __force
+#define WRITE_ONCE(x, val) \
+({							\
+	union { typeof(x) __val; char __c[1]; } __u =	\
+		{ .__val = (__force typeof(x)) (val) }; \
+	__write_once_size(&(x), __u.__c, sizeof(x));	\
+	__u.__val;					\
+})
+
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	WRITE_ONCE(prev->next, next);
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
 }
 
 static inline void list_add_tail(struct list_head *new, struct list_head *head)
@@ -109,6 +164,28 @@ void check_person_list(int reverse)
 	printf("----------PLAYER'S INFO--------------\n");
 }
 
+void delete_person(void)
+{
+	struct person *ps;
+	char name[20];
+	int delete_person_found = 0;
+
+	printf("Enter Name :");
+	scanf("%s", name);
+	list_for_each_entry(ps, &person_list, list) {
+		if (!strcmp(name, ps->name))
+		{
+			delete_person_found = 1;
+			break;
+		}
+	}
+
+	if (delete_person_found)
+		list_del(&ps->list);
+
+	printf("No this guy :(\n");
+}
+
 int main(int argc, char *argv[])
 {
 	int ch;
@@ -123,6 +200,7 @@ int main(int argc, char *argv[])
 		printf("(a)dd a player to the list\n");
 		printf("(l)ist all players' info\n");
 		printf("(r)everse print player's info\n");
+		printf("(d)elte a player\n");
 		printf("=========kenrle list==========\n");
 
 		switch (ch)
@@ -141,6 +219,9 @@ int main(int argc, char *argv[])
 				break;
 			case 'r':
 				check_person_list(1);
+				break;
+			case 'd':
+				delete_person();
 				break;
 			default:
 				break;
