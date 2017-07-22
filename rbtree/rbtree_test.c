a89970c0ac6f6a78c51c9a799aa3d1a3938c5b7e
zeroway <M_O_Bz@163.com>
2017-07-22 14:39:47 +0000

    R-B tree version 2

--- rbtree/rbtree_test.c
+++ rbtree/rbtree_test.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 #include "rbtree.h"
 
-#define CHECK_INSERT 1    // "插入"动作的检测开关(0，关闭；1，打开)
-#define CHECK_DELETE 1    // "删除"动作的检测开关(0，关闭；1，打开)
+#define CHECK_INSERT 1
+#define CHECK_DELETE 1
 #define LENGTH(a) ( (sizeof(a)) / (sizeof(a[0])) )
 
 typedef int Type;
 
 struct my_node {
-    struct rb_node rb_node;    // 红黑树节点
-    Type key;                // 键值
-    // ... 用户自定义的数据
+    struct rb_node rb_node;
+    Type key;
 };
 
-/*
- * 查找"红黑树"中键值为key的节点。没找到的话，返回NULL。
- */
+/* find out node which value equal to key */
 struct my_node *my_search(struct rb_root *root, Type key)
 {
     struct rb_node *rbnode = root->rb_node;
 
-    while (rbnode!=NULL)
+    while (rbnode != NULL)
     {
         struct my_node *mynode = container_of(rbnode, struct my_node, rb_node);
 
@@ -32,16 +29,14 @@ struct my_node *my_search(struct rb_root *root, Type key)
         else
             return mynode;
     }
-    
+
     return NULL;
 }
 
-/*
- * 将key插入到红黑树中。插入成功，返回0；失败返回-1。
- */
+/* insert a node which value equal to key to the rbtree */
 int my_insert(struct rb_root *root, Type key)
 {
-    struct my_node *mynode; // 新建结点
+    struct my_node *mynode;
     struct rb_node **tmp = &(root->rb_node), *parent = NULL;
 
     /* Figure out where to put new node */
@@ -58,9 +53,10 @@ int my_insert(struct rb_root *root, Type key)
             return -1;
     }
 
-    // 如果新建结点失败，则返回。
-    if ((mynode=malloc(sizeof(struct my_node))) == NULL)
-        return -1; 
+	/* out of memeory, failed and return */
+    if ((mynode = malloc(sizeof(struct my_node))) == NULL)
+        return -1;
+
     mynode->key = key;
 
     /* Add new node and rebalance tree. */
@@ -70,79 +66,88 @@ int my_insert(struct rb_root *root, Type key)
     return 0;
 }
 
-/* 
- * 删除键值为key的结点
- */
+/* Delete the node which value equal to key */
 void my_delete(struct rb_root *root, Type key)
 {
     struct my_node *mynode;
 
-    // 在红黑树中查找key对应的节点mynode
+	/* find out the node which value equal to key */
     if ((mynode = my_search(root, key)) == NULL)
         return ;
 
-    // 从红黑树中删除节点mynode
+	/* erase the node from rbtree */
     rb_erase(&mynode->rb_node, root);
     free(mynode);
 }
 
-/*
- * 打印"红黑树"
- */
+/* print out rbtree */
 static void print_rbtree(struct rb_node *tree, Type key, int direction)
 {
+	/*
+	 * find out the current node's parent node
+	 * rb_parent(tree) return a parent node point
+	 * container_of find out the parent's data struct
+	 */
+	struct my_node *parent = container_of(rb_parent(tree), struct my_node, rb_node);
+
     if(tree != NULL)
-    {   
-        if(direction==0)    // tree是根节点
+    {
+		/* root node */
+        if(0 == direction)
             printf("%2d(B) is root\n", key);
-        else                // tree是分支节点
-            printf("%2d(%s) is %2d's %6s child\n", key, rb_is_black(tree)?"B":"R", key, direction==1?"right" : "left");
+        else /* branch node */
+            printf("%2d(%s) is %2d's %6s child\n", key, rb_is_black(tree) ? "B":"R", parent->key, direction == 1 ? "right" : "left");
 
         if (tree->rb_left)
             print_rbtree(tree->rb_left, rb_entry(tree->rb_left, struct my_node, rb_node)->key, -1);
+
         if (tree->rb_right)
-            print_rbtree(tree->rb_right,rb_entry(tree->rb_right, struct my_node, rb_node)->key,  1); 
-    }   
+            print_rbtree(tree->rb_right,rb_entry(tree->rb_right, struct my_node, rb_node)->key,  1);
+    }
 }
 
 void my_print(struct rb_root *root)
 {
     if (root!=NULL && root->rb_node!=NULL)
-        print_rbtree(root->rb_node, rb_entry(root->rb_node, struct my_node, rb_node)->key,  0); 
+        print_rbtree(root->rb_node, rb_entry(root->rb_node, struct my_node, rb_node)->key,  0);
 }
 
-
-void main()
+int main(int argc, char *argv[])
 {
-    int a[] = {10, 40, 30, 60, 90, 70, 20, 50, 80};
-    int i, ilen=LENGTH(a);
+    int raw_data[] = {10, 40, 30, 60, 90, 70, 20, 50, 80};
+    int i;
+	int len;
     struct rb_root mytree = RB_ROOT;
 
+	len = LENGTH(raw_data);
+
     printf("== 原始数据: ");
-    for(i=0; i<ilen; i++)
-        printf("%d ", a[i]);
+    for(i = 0; i < len; i++)
+        printf("%d ", raw_data[i]);
     printf("\n");
 
-    for (i=0; i < ilen; i++) 
+    for(i = 0; i < len; i++)
     {
-        my_insert(&mytree, a[i]);
+        my_insert(&mytree, raw_data[i]);
 #if CHECK_INSERT
-        printf("== 添加节点: %d\n", a[i]);
+        printf("== 添加节点: %d\n", raw_data[i]);
         printf("== 树的详细信息: \n");
         my_print(&mytree);
         printf("\n");
 #endif
-
     }
 
 #if CHECK_DELETE
-    for (i=0; i<ilen; i++) {
-        my_delete(&mytree, a[i]);
+    for(i = 0; i < len; i++)
+	{
+        my_delete(&mytree, raw_data[i]);
 
-        printf("== 删除节点: %d\n", a[i]);
+        printf("== 删除节点: %d\n", raw_data[i]);
         printf("== 树的详细信息: \n");
         my_print(&mytree);
         printf("\n");
     }
 #endif
+
+	return 0;
 }
