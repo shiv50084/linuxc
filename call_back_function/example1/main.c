7ea4a414a7eb14c9b87dc695f7d0b5ddf62f1567
zeroway <M_O_Bz@163.com>
2018-10-17 17:26:27 +0800

    Handle the void* corrective

--- call_back_function/example1/main.c
+++ call_back_function/example1/main.c
@@ -6,22 +6,29 @@ static int cnt2 = 0;
 
 void say_hello(void *str)
 {
+	/* void * ==> char * */
 	printf("Hello %s, %d times\n", (const char *)str, cnt2++);
 }
 
 void count_number(void *num)
 {
+	/*
+	 * void * ==> int *
+	 * 再对int *取值
+	 */
 	int i;
 
 	printf("cnt = %d\n", cnt++);
-	for (i = 0; i < (int)num; i++)
+	for (i = 0; i < *(int *)num; i++)
 		printf("%d ", i);
 	printf("\n");
 }
 
 int main(int argc, char *argv[])
 {
-	repeat_three_times(say_hello, (void *)"zeroway");
-	repeat_three_times(count_number, (void *)4);
+	int repeat_time = 4;
+	const char *name = "anonymous";
+	repeat_three_times(say_hello, (void *)name); /* char * ==> void **/
+	repeat_three_times(count_number, (void *)&repeat_time); /* int * == > void **/
 	return 0;
 }
