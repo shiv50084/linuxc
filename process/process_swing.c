390bc5e00c5d8cb450cdd72a473b352869fce313
zeroway <M_O_Bz@163.com>
2019-05-08 22:27:33 +0000

    Process : create process link and swing

---
+++ process/process_swing.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*
+ * create a process swing as below
+ *		  parent
+ *	  /		 |    \
+ *	child	child	child
+ *
+ * using `pstree` to check
+ */
+int main(int argc, char *argv[])
+{
+	int counter = 0;
+	int i;
+	pid_t pid;
+
+	if (argc < 2)
+		counter = 2;
+	else
+		counter = atoi(argv[1]);
+
+	/* i = 1, ignore the main proces */
+	for (i = 1; i < counter; i++)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork error");
+			exit(1);
+		}
+		else if (pid > 0)
+		{
+			/* parent continue fork loop */
+			printf("This is parent pid: %d, my child id: %d\n", getpid(), pid);
+		}
+		else
+		{
+			/* child exit the loop */
+			printf("This is child pid: %d, my parent id: %d\n", getpid(), getppid());
+			break;
+		}
+	}
+
+	while (1)
+		sleep(2);
+
+	return 0;
+}
