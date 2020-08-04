9c78a7fd64633c2e4d2dbe7c92b7061958cd34b8
zeroway <M_O_Bz@163.com>
2020-08-04 14:10:20 +0800

    Avoid bitrot in C macros
    Change-Id: I1fa33eab1b42d54e45ad9eed89cc0185efc8ba2c

--- misc/debug_message.c
+++ misc/debug_message.c
@@ -2,10 +2,12 @@
 
 /* Code from LinuxProgramingByExample book */
 
+#define TECHNIQUE4
+#define ENABLE_DEBUG 0
+
 /* TECHINQUE 1 SUCK don't care */
 
 #ifdef TECHNIQUE2
-
 /* TECHNIQUE 2, most portable, recommended */
 /* In application header file: */
 #ifdef MYAPPDEBUG
@@ -13,9 +15,9 @@
 #else
 #define DPRINT(stuff)
 #endif
+#endif
 
-#else
-
+#ifdef TECHNIQUE3
 /* TECHNIQUE 3, cleanest, but C99 only */
 /* In application header file: */
 #ifdef MYAPPDEBUG
@@ -23,7 +25,19 @@
 #else
 #define DPRINT(mesg, ...)
 #endif
+#endif
 
+/*
+ * The best approach
+ * Avoid bitrot in C macros
+ * http://blog.vmsplice.net/2020/07/avoiding-bitrot-in-c-macros.html
+ */
+#ifdef TECHNIQUE4
+#define DPRINT(mesg, ...) do { \
+	if (ENABLE_DEBUG) { \
+		fprintf(stderr, mesg, ## __VA_ARGS__); \
+	} \
+} while (0)
 #endif
 
 /*
@@ -34,9 +48,25 @@
 int main(int argc, char *argv[])
 {
 #ifdef TECHNIQUE2
-	DPRINT((stderr, "Technique 2 var = %d\n", 911));
-#else
-	DPRINT("Technique 3 var = %d\n", 110);
+	int r = 911;
+
+	/*
+	 * what if rename r to radius ?
+	 * could compiler find the syntax error ?
+	 */
+	DPRINT((stderr, "Technique 2 var = %d\n", r));
+#endif
+
+#ifdef TECHNIQUE3
+	int r = 911;
+
+	DPRINT("Technique 3 var = %d\n", r);
+#endif
+
+#ifdef TECHNIQUE4
+	int r = 911;
+
+	DPRINT("Technique 4 var = %d\n", r);
 #endif
 	return 0;
 }
